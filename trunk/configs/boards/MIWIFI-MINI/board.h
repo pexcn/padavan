1469fa7b332ef63a9e47fce9e4e23f9cb08ecc4d
pexcn <i@pexcn.me>
2019-09-02 18:13:59 +0800

    add xiaomi miwifi mini support

---
+++ trunk/configs/boards/MIWIFI-MINI/board.h
@@ -0,0 +1,32 @@
+/* MIWIFI MINI */
+
+#define BOARD_PID		"MIWIFI-MINI"
+#define BOARD_NAME		"MIWIFI-MINI"
+#define BOARD_DESC		"XiaoMi MiWiFi Mini Wireless Router"
+#define BOARD_VENDOR_NAME	"XiaoMi Inc."
+#define BOARD_VENDOR_URL	"https://www.mi.com"
+#define BOARD_MODEL_URL		"https://www.mi.com/miwifimini/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_RESET
+#define BOARD_GPIO_BTN_WPS	30
+#undef  BOARD_GPIO_BTN_WLTOG
+#undef BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	24
+#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER	29
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	26
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
+
