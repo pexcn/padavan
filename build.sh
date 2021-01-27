db67fcbac651d1e0627825342cef661821aca18f
pexcn <i@pexcn.me>
2021-01-27 12:52:04 +0800

    Revert "use pre-compiled toolchain for build"
    This reverts commit b4505f6ad405daefbf061d30c4ee6b98b3ac4bed.

--- build.sh
+++ build.sh
@@ -12,12 +12,8 @@ update_config() {
 
 build_toolchain() {
   cd toolchain-mipsel
-  #sudo ./clean_sources
-  #sudo ./build_toolchain
-  local toolchain_dir="toolchain-3.4.x"
-  local toolchain_url="https://github.com/pexcn/padavan-hanwckf/releases/download/toolchain-v1.1/mipsel-linux-uclibc.tar.xz"
-  mkdir $toolchain_dir
-  curl -sSL $toolchain_url | tar -Jxf - -C $toolchain_dir
+  sudo ./clean_sources
+  sudo ./build_toolchain
   cd ..
 }
 
